cb336fe1bb520937f7b8c11882f940544a90aa3e
skhan75 <sami.ahmadkhan12@gmail.com>
2023-08-21 12:55:59 -0700

    dct init and cleanup

--- src/codecs/video/core/dct.c
+++ src/codecs/video/core/dct.c
@@ -1,16 +1,12 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define BLOCK_SIZE 8
 #define PI 3.14159265358979323846
-
-double DCT_MATRIX[BLOCK_SIZE][BLOCK_SIZE];
 
 typedef struct {
     int block_size;
     double **dct_matrix;
 } DCTContext;
-
 
 // P.S This is a foundational DCT implementation. The real magic and complexity come with optimizations 
 // like fast cosine transforms, fixed-point arithmetic, SIMD instructions, and other hardware/software 
@@ -27,31 +23,55 @@ static void generate_dct_matrix(DCTContext *ctx) {
     }
 }
 
-void perform_dct(double block[BLOCK_SIZE][BLOCK_SIZE], double output[BLOCK_SIZE][BLOCK_SIZE]) {
-    double temp[BLOCK_SIZE][BLOCK_SIZE] = {0};
+DCTContext *init_dct(int block_size) {
+    DCTContext *ctx = (DCTContext *)malloc(sizeof(DCTContext));
+    if(!ctx) return NULL;
+
+    ctx->block_size = block_size;
+    ctx->dct_matrix = (double **)malloc(sizeof(double *) * block_size);
+    for(int i=0; i<block_size; i++) {
+        ctx->dct_matrix[i] = (double *)malloc(sizeof(double) * block_size);
+    }
+
+    generate_dct_matrix(ctx);
+    return ctx;
+}
+
+void free_dct(DCTContext *ctx) {
+    for (int i = 0; i < ctx->block_size; i++) {
+        free(ctx->dct_matrix[i]);
+    }
+    free(ctx->dct_matrix);
+    free(ctx);
+}
+
+void perform_dct(DCTContext *ctx, double block[][ctx->block_size], double output[][ctx->block_size]) {
+    double temp[ctx->block_size][ctx->block_size];
 
     // Multiply the block with the DCT matrix = DCT_MATRIX * block
-    for(int i=0; i<BLOCK_SIZE; i++) {
-        for(int j=0; j<BLOCK_SIZE; j++) {
-            for(int k=0; k<BLOCK_SIZE; k++) {
-                temp[i][j] += DCT_MATRIX[i][k] * block[k][j];
+    for (int i = 0; i < ctx->block_size; i++) {
+        for (int j = 0; j < ctx->block_size; j++) {
+            temp[i][j] = 0;
+            for (int k = 0; k < ctx->block_size; k++) {
+                temp[i][j] += ctx->dct_matrix[i][k] * block[k][j];
             }
         }
     }
 
     // Multiply the result with the transpose of the DCT matrix = temp * DCT_MATRIX^T
-    for (int i = 0; i < BLOCK_SIZE; i++) {
-        for (int j = 0; j < BLOCK_SIZE; j++) {
-            for (int k = 0; k < BLOCK_SIZE; k++) {
-                output[i][j] += temp[i][k] * DCT_MATRIX[k][j];
+    for (int i = 0; i < ctx->block_size; i++) {
+        for (int j = 0; j < ctx->block_size; j++) {
+            output[i][j] = 0;
+            for (int k = 0; k < ctx->block_size; k++) {
+                output[i][j] += temp[i][k] * ctx->dct_matrix[k][j];
             }
         }
     }
 }
 
-void quantize_dct_coefficients(double coefficients[BLOCK_SIZE][BLOCK_SIZE], int quantization_table[BLOCK_SIZE][BLOCK_SIZE]) {
-    for (int i = 0; i < BLOCK_SIZE; i++) {
-        for (int j = 0; j < BLOCK_SIZE; j++) {
+void quantize_dct_coefficients(DCTContext *ctx, double coefficients[][ctx->block_size], int quantization_table[][ctx->block_size]) {
+    for (int i = 0; i < ctx->block_size; i++) {
+        for (int j = 0; j < ctx->block_size; j++) {
             coefficients[i][j] /= quantization_table[i][j];
             coefficients[i][j] = round(coefficients[i][j]);
         }
