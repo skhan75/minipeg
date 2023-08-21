d987f1cc4fb0e38befe1947283dbc86e80dbafe8
skhan75 <sami.ahmadkhan12@gmail.com>
2023-08-21 12:52:02 -0700

    instead of global variables for dct matrix, encapsulate with structs

--- src/codecs/video/core/dct.c
+++ src/codecs/video/core/dct.c
@@ -6,17 +6,23 @@
 
 double DCT_MATRIX[BLOCK_SIZE][BLOCK_SIZE];
 
+typedef struct {
+    int block_size;
+    double **dct_matrix;
+} DCTContext;
+
+
 // P.S This is a foundational DCT implementation. The real magic and complexity come with optimizations 
 // like fast cosine transforms, fixed-point arithmetic, SIMD instructions, and other hardware/software 
 // optimizations.
 
 // Initialize the DCT matrix
-static void initialize_dct_matrix() {
+static void generate_dct_matrix(DCTContext *ctx) {
     double scaleFactor = sqrt(2.0 / BLOCK_SIZE);
-    for(int i=0; i<BLOCK_SIZE; i++) {
-        for(int j=0; j<BLOCK_SIZE; j++) {
-            if(i==0) DCT_MATRIX[i][j] = 1.0 / sqrt(BLOCK_SIZE);
-            else DCT_MATRIX[i][j] = scaleFactor * cos((PI/(2.0*BLOCK_SIZE)) * j*(2.0*i+1));
+    for(int i=0; i<ctx->block_size; i++) {
+        for(int j=0; j<ctx->block_size; j++) {
+            if(i==0) ctx->dct_matrix[i][j] = 1.0 / sqrt(ctx->block_size);
+            else ctx->dct_matrix[i][j] = scaleFactor * cos((PI/(2.0*ctx->block_size)) * j*(2.0*i+1));
         }
     }
 }
